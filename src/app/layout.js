4696e0ddb2269b5ab09191e9954c87b272515e38
xxecond <andrewsampadu9@gmail.com>
2025-10-03 04:27:57 +0000

    Project update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import { Header, Footer } from "@/components";
 import Script from "next/script";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,28 +16,37 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Andrews Ampadu",
   description: "My Portfolio",
-  icons:{
+  icons: {
     icon: "/favicon.ico",
-  }
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="8dhQaWLvSrNA8d3hvZLdryEfs4q3EjiQeohrdVsHBYs" />
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-LFBY2BJJ4F"></Script>
-<Script id="ga-script" strategy="afterInteractive">
-  {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LFBY2BJJ4F')`};
-</Script>
+        <meta
+          name="google-site-verification"
+          content="8dhQaWLvSrNA8d3hvZLdryEfs4q3EjiQeohrdVsHBYs"
+        />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LFBY2BJJ4F"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LFBY2BJJ4F');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><Header />
+      >
+        <Header />
         {children}
         <Footer />
       </body>
